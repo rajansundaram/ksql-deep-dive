fcdad9d4c5e4a3ddda54edf9be8e64933432e4f7
Brijesh Jaggi <brijesh.jaggi@gmail.com>
2022-07-07 15:18:07 -0400

    Multi Key join

--- Ex2-multikey-strategy/example-2.sql
+++ Ex2-multikey-strategy/example-2.sql
@@ -1,7 +1,7 @@
 # Multi column keys 
 
 
-## Creating a multi column key (Option 1)
+## Creating a multi column key in the definition (Option 1)
 
 drop stream orders_multi_key;
 
@@ -13,7 +13,7 @@ CREATE STREAM orders_multi_key (orderid STRING , customerid STRING KEY, itemid S
           
 
 
-DROP stream items_multi_key;
+DROP stream  items_multi_key;
 CREATE TABLE items_multi_key (itemid STRING PRIMARY KEY,customerid STRING PRIMARY KEY, itemname STRING)
     WITH (KAFKA_TOPIC='items_multi_key',
           VALUE_FORMAT='json',KEY_FORMAT='json',
@@ -34,11 +34,64 @@ INSERT INTO orders_multi_key VALUES ('abc123', '1', '101', '2020-05-01');
 select * from orders_multi_key emit changes;
 
 
-## Creating a multicolumn key ( Option 2)
+## Creating a multicolumn key using PARTITION BY ( Option 2)
+
+DROP STREAM IF EXISTS orders ;
+CREATE OR REPLACE STREAM orders (orderid STRING KEY, customerid STRING , itemid STRING , purchasedate STRING)
+    WITH (KAFKA_TOPIC='orders',
+          VALUE_FORMAT='json', KEY_FORMAT='json',
+          PARTITIONS=6);	
+
+INSERT INTO orders VALUES ('abc123', '1', '101', '2020-05-01');
+INSERT INTO orders VALUES ('abc345', '1', '102', '2020-05-01');
+INSERT INTO orders VALUES ('abc678', '2', '101', '2020-05-01');
+
+### NOW LETS CHANGE AND HAVE MULTIPLE KEYS
+DROP STREAM IF EXISTS orders_multikey ;
+CREATE OR REPLACE STREAM orders_multikey  AS
+    SELECT orderid, customerid, itemid , purchasedate
+    FROM ORDERS
+    PARTITION BY customerid, itemid;
+
+NOTE : MULTIPLE KEYS CAN BE ADDED USING THIS SYNTAX, HOWEVER DATA CANNOT BE JOINED WITH MULTIPLE KEYS AND MULTIPLE KEYS SHOULD BE MOVED TO A STRUCT AS SHOWN IN THE NEXT EXAMPLE.
+
+
+## Creating a multicolumn key using PARTITION BY & STRUCT ( Option 3)
+
+DROP STREAM IF EXISTS orders ;
+CREATE OR REPLACE STREAM orders (orderid STRING , customerid STRING KEY, itemid STRING KEY, purchasedate STRING)
+    WITH (KAFKA_TOPIC='orders',
+          VALUE_FORMAT='json', KEY_FORMAT='json',
+          PARTITIONS=6);	
+
+INSERT INTO orders VALUES ('abc123', '1', '101', '2020-05-01');
+INSERT INTO orders VALUES ('abc345', '1', '102', '2020-05-01');
+INSERT INTO orders VALUES ('abc678', '2', '101', '2020-05-01');
+
+DROP STREAM IF EXISTS orders_multikey_struct ;
+CREATE OR REPLACE STREAM orders_multikey_struct  AS
+    SELECT orderid, customerid, itemid , purchasedate
+    FROM ORDERS
+    PARTITION BY STRUCT (customerid, itemid);
+
+DROP TABLE IF EXISTS items ;
+CREATE OR REPLACE TABLE items (itemid STRING PRIMARY KEY, customerid STRING PRIMARY KEY, itemname STRING)
+    WITH (KAFKA_TOPIC='items',
+          VALUE_FORMAT='json', KEY_FORMAT='json',
+          PARTITIONS=6);
+
+INSERT INTO items VALUES ('101', '1', 'Television 60-in');
+INSERT INTO items VALUES ('102','2', 'Laptop 15-in');
 
 
 
-## Creating a multicolumn key ( Option 3)
+CREATE OR REPLACE STREAM orders_enriched_multikey AS
+  SELECT 
+         orders.orderid, orders.purchasedate,
+         items.itemid, items.itemname
+  FROM orders
+  LEFT JOIN customers on orders.customerid = customers.customerid 
+  LEFT JOIN items on orders.itemid = items.itemid 
 
 
 
