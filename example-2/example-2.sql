1b5d651ec366490a146f4fe2a30abbb8f7ccb6fb
Brijesh Jaggi <brijesh.jaggi@gmail.com>
2022-07-07 12:13:05 -0400

    multi key topics and joins

--- example-2/example-2.sql
+++ example-2/example-2.sql
@@ -10,5 +10,39 @@ a join on a condition such as stream1.key_col = my_other_stream.key_col.
 
 
 
+
+drop stream orders_multi_key;
+
+DROP stream orders_multi_key;
+CREATE STREAM orders_multi_key (orderid STRING , customerid STRING KEY, itemid STRING KEY, purchasedate STRING)
+    WITH (KAFKA_TOPIC='orders_multi_key',
+          VALUE_FORMAT='json',KEY_FORMAT='json',
+          PARTITIONS=6);
+          
+
+
+DROP stream items_multi_key;
+CREATE TABLE items_multi_key (itemid STRING PRIMARY KEY,customerid STRING PRIMARY KEY, itemname STRING)
+    WITH (KAFKA_TOPIC='items_multi_key',
+          VALUE_FORMAT='json',KEY_FORMAT='json',
+          PARTITIONS=6);
+
+
+CREATE TABLE items_multi_key_struct (MY_KEY STRUCT<itemid STRING, customerid STRING> PRIMARY KEY, itemname STRING)
+    WITH (KAFKA_TOPIC='items_multi_key_struct',
+          VALUE_FORMAT='json',KEY_FORMAT='json',
+          PARTITIONS=6);
+
+
+INSERT INTO items_multi_key_struct VALUES (STRUCT(itemid:='101',customerid:='1'),'Television 60-in'); 
+select * from items_multi_key emit changes;
+
+
+INSERT INTO orders_multi_key VALUES ('abc123', '1', '101', '2020-05-01');
+select * from orders_multi_key emit changes;
+
+
+
+
 https://www.confluent.io/blog/ksqldb-0-15-reads-more-message-keys-supports-more-data-types/#multiple-key-columns
 
