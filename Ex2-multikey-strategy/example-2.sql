abb960105361f7b00745f49a65457ece6eacbc79
Brijesh Jaggi <brijesh.jaggi@gmail.com>
2022-07-07 12:37:50 -0400

    Multi Key join

--- Ex2-multikey-strategy/example-2.sql
+++ Ex2-multikey-strategy/example-2.sql
@@ -1,12 +1,5 @@
 # Multi column keys 
 
-CREATE STREAM my_other_stream (
-    key_col STRUCT<k1 STRING, k2 BOOLEAN> KEY, v1 STRING, v2 INT 
-) WITH (KAFKA_TOPIC='my_topic', FORMAT='JSON');
-
-
-a join on a condition such as stream1.key_col = my_other_stream.key_col.
-
 
 ## Creating a multi column key (Option 1)
 
