1408fb158a41ea471c3ec071d8bb1494dc5a5929
Brijesh Jaggi <brijesh.jaggi@gmail.com>
2022-07-12 12:10:16 -0400

    OUTER JOIN CODE

--- Ex1-join-table-streams/example-1.sql
+++ Ex1-join-table-streams/example-1.sql
@@ -1,18 +1,20 @@
 
 
-# Join Multiple tables ( 3 & 4 )
 
-## Initialize the project
+## Initialize the project with the  customer & items table  a stream
+DROP TABLE IF EXISTS customers;
 CREATE TABLE customers (customerid STRING PRIMARY KEY, customername STRING)
     WITH (KAFKA_TOPIC='customers',
           VALUE_FORMAT='json',
           PARTITIONS=6);
 
+DROP TABLE IF EXISTS items;
 CREATE TABLE items (itemid STRING PRIMARY KEY, itemname STRING)
     WITH (KAFKA_TOPIC='items',
           VALUE_FORMAT='json',
           PARTITIONS=6);
 
+# create a Stream of orders;
 CREATE STREAM orders (orderid STRING KEY, customerid STRING, itemid STRING, purchasedate STRING)
     WITH (KAFKA_TOPIC='orders',
           VALUE_FORMAT='json',
@@ -45,11 +47,18 @@ INSERT INTO orders VALUES ('xyz987', '2', '102', '2020-05-05');
 ## 
 SET 'auto.offset.reset' = 'earliest';
 
+# Join one stream with 2 tables
+DROP STREAM IF EXISTS orders_enriched;
+CREATE OR REPLACE STREAM orders_enriched AS
+  SELECT customers.customerid AS customerid, customers.customername AS customername,
+         orders.orderid, orders.purchasedate,
+         items.itemid, items.itemname, items_french.itemname_french
+  FROM orders
+  LEFT JOIN customers on orders.customerid = customers.customerid
+  LEFT JOIN items on orders.itemid = items.itemid; 
 
 
-
-
-## 4 Way join
+# and now 4 Way join ( one stream with 3 tables)
 
 CREATE TABLE items_french (itemid STRING PRIMARY KEY, itemname_french STRING)
     WITH (KAFKA_TOPIC='items_french',
@@ -62,7 +71,7 @@ INSERT INTO items_french VALUES ('102', 'Portable 15-in');
 INSERT INTO items_french VALUES ('103', 'Haut-parleurs');
 
 
-### 4 way LEFT join Query
+# 4 way LEFT join Query
 CREATE OR REPLACE STREAM orders_enriched_bilingual AS
   SELECT customers.customerid AS customerid, customers.customername AS customername,
          orders.orderid, orders.purchasedate,
@@ -80,9 +89,6 @@ DROP TABLE IF EXISTS ITEMS DELETE TOPIC;
 DROP TABLE IF EXISTS ITEMS_FRENCH DELETE TOPIC;
 DROP TABLE IF EXISTS CUSTOMERS DELETE TOPIC;
 DROP STREAM IF EXISTS ORDERS DELETE TOPIC;
-
-
-
 
 # References :
 https://developer.confluent.io/tutorials/multi-joins/ksql.html
