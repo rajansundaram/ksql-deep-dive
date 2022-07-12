d151e235639ca362d08c7aacbd6d1d7dbcaed533
Brijesh Jaggi <brijesh.jaggi@gmail.com>
2022-07-12 11:42:21 -0400

    formatted code for inner joins

--- Ex1-join-table-streams/INNER-JOIN-N-Tables/example-1.sql
+++ Ex1-join-table-streams/INNER-JOIN-N-Tables/example-1.sql
@@ -1,3 +1,6 @@
+
+# Create a table ITEMS
+DROP TABLE IF EXISTS items;
 CREATE TABLE items (id VARCHAR PRIMARY KEY, make VARCHAR, model VARCHAR, unit_price DOUBLE)
 WITH (KAFKA_TOPIC='items', VALUE_FORMAT='avro', PARTITIONS=1);
 
@@ -7,17 +10,12 @@ INSERT INTO items VALUES('item_4', 'Wilson', 'NCAA Replica', 29.99);
 INSERT INTO items VALUES('item_7', 'SKLZ', 'Control Training', 49.99);
 
 
-DROP STREAM ny_orders;
-DROP STREAM orders;
 
+# Create a stream ORDERS
+DROP STREAM IF EXISTS orders;
 CREATE STREAM orders (ordertime BIGINT, orderid INTEGER, itemid VARCHAR, orderunits INTEGER)
 WITH (KAFKA_TOPIC='item_orders', VALUE_FORMAT='avro', PARTITIONS=1);
 
-CREATE STREAM orders_enriched AS
-SELECT o.*, i.*, 
-	o.orderunits * i.unit_price AS total_order_value
-FROM orders o 
-JOIN items i on o.itemid = i.id;
 
 INSERT INTO orders VALUES (1620501334477, 65, 'item_7', 5);
 INSERT INTO orders VALUES (1620502553626, 67, 'item_3', 2);
@@ -27,30 +25,33 @@ INSERT INTO orders VALUES (1620505321941, 74, 'item_7', 3);
 INSERT INTO orders VALUES (1620506437125, 72, 'item_7', 9);
 INSERT INTO orders VALUES (1620508354284, 73, 'item_3', 4);
 
+# EXAMPLE OF A STREAM JOIN TABLE
+
+CREATE STREAM orders_enriched AS
+SELECT o.*, i.*, 
+	o.orderunits * i.unit_price AS total_order_value
+FROM orders o 
+INNER JOIN items i on o.itemid = i.id;
 
 SELECT * FROM orders_enriched EMIT CHANGES;
 
 
+# LETS TRY WITH STREAM JOIN 2 TABLES 
 CREATE TABLE items_category (id VARCHAR PRIMARY KEY, category STRING)
-WITH (KAFKA_TOPIC='items', VALUE_FORMAT='avro', PARTITIONS=1);
-
+WITH (KAFKA_TOPIC='items_category', VALUE_FORMAT='avro', PARTITIONS=1);
 
 INSERT INTO items_category VALUES('item_3', 'BasketBall');
 INSERT INTO items_category VALUES('item_4', 'BasketBall');
 INSERT INTO items_category VALUES('item_7', 'BasketBall');
 
-## NOT POSSIBLE - CHECK with #ksql
+
+## EXAMPLE OF INNER JOIN 1 STREAM with 2 TABLES
 CREATE STREAM orders_enriched_with_category AS
 SELECT o.*, i.*, ic.category 
 FROM orders o 
-JOIN items i on o.itemid = i.id 
-JOIN items_category ic on o.itemid =ic.id;
+INNER JOIN items i on o.itemid = i.id 
+INNER JOIN items_category ic on o.itemid =ic.id;
 
-#works with a second level join 
-# 3 level of join
-CREATE STREAM orders_enriched_with_category AS
-SELECT o.*, ic.category 
-FROM orders_enriched o
-JOIN items_category ic on o.O_ITEMID =ic.id;
+
 
 
---
+++ Ex1-join-table-streams/README.md
@@ -0,0 +1,12 @@
+
+# Streams Join Tables ( N Way )
+
+Here are some hands-on labs to demonstrate how streams and joins can be used for joins.
+
+
+This lab is broken into INNER Join and LEFT Join.
+
+Here is the official documentation for Joins [Joins in ksqlDB] https://docs.ksqldb.io/en/latest/developer-guide/joins/
+
+Also refer the deck for a quick review of important material [deck] https://docs.google.com/presentation/d/16Dnp4uxdzIo-9C8paP0uhJjOWvNIWb-DEbq6kw1GcRM/edit#slide=id.g125be447d3c_0_0
+
